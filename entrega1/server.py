cf021bf849ea037e13bd60fcdde12b0114acadc0
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2023-08-04 17:54:42 -0300

    Update server.py

--- entrega1/server.py
+++ entrega1/server.py
@@ -1,21 +1,50 @@
 import socket
+import select
+import sys
+import time
 
-HOST = "192.110.0.101" # ifconfig pra obter ip da maquina na qual vai rodar - igual ao client.py
-PORT = 5000 # pode ser outra, mas tem que ser igual à porta do client.py
-BUFFERSIZE = 1024
+IP = "192.101.0.202" # TROCAR
+PORTA_ENTRADA = 5000
+PORTA_SAIDA = 9999
+timeout = 3
+buf = 1024
 
-resposta = "Mensagem recebida pelo servidor"
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((IP, PORTA_ENTRADA))
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind((HOST,PORT))
+fim = False # arquivo recebido até o fim
 
-# server.listen()
+while not fim:
+    data, addr = sock.recvfrom(1024)
+    if data:
+        print(f"Mensagem: {data}")
+        file_name = data.strip()
 
-while True:
-    tupla = server.recvfrom(BUFFERSIZE)
-    mensagem = tupla[0]
-    endereco = tupla[1]
-    #print(endereco)
-    print(f"origem: {endereco}")
-    print(f"message: {mensagem}")
-    server.sendto(resposta.encode("utf-8"), endereco)
+    f = open(file_name, 'wb')
+
+    while True:
+        ready = select.select([sock], [], [], timeout)
+        if ready[0]:
+            data, addr = sock.recvfrom(1024)
+            f.write(data)
+        else:
+            print(f"{file_name} lido com sucesso")
+            f.close()
+            fim = True
+            break
+
+sock.close()
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock2.sendto(file_name, (IP, PORTA_SAIDA))
+
+print(f"Enviando {file_name} ...")
+
+f = open(file_name, "r")
+data2 = f.read(buf)
+while(data2):
+    if(sock.sendto(data2, (IP, PORTA_SAIDA))):
+        data2 = f.read(buf)
+        time.sleep(0.02) # Give receiver a bit time to save
+
+sock2.close()
+f.close()
