eabf99e10d9c9f7bc350e6d2969277c58ae57365
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2023-09-15 19:57:55 -0300

    Add files via upload
    versão 100% completa da funcionalidade MAS sem o RDT. usar de base depois de desenvolver rdt

---
+++ bb-client.py
@@ -0,0 +1,56 @@
+import socket
+import threading
+import random
+import re
+
+# socket udp
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# aloca porta aleatória nessa faixa
+client.bind(("localhost", random.randint(5000, 9900)))
+
+# inicialmente pede nome do usuário
+name = input("Nome: ")
+
+# dicionario de contatos
+contatos = {}
+
+
+
+# essa função atualiza lista local de contatos a cada nova mensagem - mapeamento (ip,porta):nome
+def tentativa_add(message):
+	if (message.startswith("127.0.0.1:")):
+		lista_nome = message.split()
+		porta = int(lista_nome[0][10:14])
+		nome = ""
+		for i in range(16,len(lista_nome[0])):
+			if (lista_nome[0][i] != ':'):
+				nome += lista_nome[0][i]
+			else:
+				break
+		ip = '127.0.0.1'
+		endereco = (ip, porta)
+		contatos[endereco] = nome
+
+
+def receive():
+	while True:
+		try:
+			message, _ = client.recvfrom(1024)
+			mstring = message.decode()
+			print(mstring)
+			tentativa_add(mstring)
+		except:
+			pass	
+
+# eternamente esperando mensagens, thread pra isso
+t = threading.Thread(target=receive)
+t.start()
+
+# inicialmente, manda nome para servidor (uma única vez)
+client.sendto(f"NOME: {name}".encode(), ("localhost", 9999))
+
+# continuamente pede input do usuário, manda pra porta 9999 que é a do servidor (arbitrário)
+while True:
+	message = input("")
+	client.sendto(message.encode(), ("localhost", 9999))
+
---
+++ bb-server.py
@@ -0,0 +1,66 @@
+import socket
+import threading
+import queue
+from datetime import datetime
+
+# estrutura para armazenar mensagens
+messages = queue.Queue()
+clients = {}
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind(("localhost",9999))
+
+
+def receive():
+	while True:
+		try:
+			message, addr = server.recvfrom(1024)
+			messages.put((message, addr))
+		except:
+			pass
+
+# envia para todos os contatos - tanto mensagens normais quanto comandos cadastrar/descadastrar e lista usuários
+def broadcast():
+	while True:
+		while not messages.empty():
+			message, addr = messages.get()
+			print(message.decode())
+				
+			# se for pedido de lista, não dá broadcast, só envia lista pra quem pediu
+			if message.decode() == "list":
+				print("lista requisitada")
+				for client,client_name in clients.items():
+					server.sendto(f"nome: {client_name} | endereço: {client}".encode(), addr)
+
+			# tirar cliente da sala
+			elif message.decode() == "bye":
+				clients.pop(addr)
+
+			# notificar todos sobre chegada de novo usuário
+			elif message.decode().startswith("NOME:"):
+				string_nome = message.decode().split()[1:]
+				name = ""
+				for i in range(len(string_nome)):
+					name += str(string_nome[i])
+					if (i != len(string_nome)-1):
+						name += " "
+					clients[addr] = name
+					for client, client_name in clients.items():
+							server.sendto(f"{name} entrou na sala! endereço: {addr}".encode(), client)
+			
+			# mandar mensagem normal no chat
+			else:
+				name = clients[addr]
+				for client, client_name in clients.items():
+						string_mensagem = message.decode()
+						dt = datetime.now()
+						formatted_time = dt.strftime("%H:%M:%S %d/%m/%Y")
+						server.sendto(f"{addr[0]}:{addr[1]}/~{name}: {string_mensagem} {formatted_time}".encode(),client)
+
+
+# threads pra manter transmissão e recepção coerente
+t1 = threading.Thread(target=receive)
+t2 = threading.Thread(target=broadcast)
+
+t1.start()
+t2.start()
